a1a2f140e3ad6ee6d3802c728061f8a79695b7e9
Rhuan Coltre <rhuancoltre@gmail.com>
2015-10-30 21:12:59 -0200

    Comit Inicial, Banco de dados, MVC, Filtro de Conexao,

---
+++ src/java/br/grupointegrado/ads/picaretas/controle/LoginServlet.java
@@ -0,0 +1,11 @@
+package br.grupointegrado.ads.picaretas.controle;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ *
+ * @author Rhuan Coltre
+ */
+public class LoginServlet extends HttpServlet {
+
+}
---
+++ src/java/br/grupointegrado/ads/picaretas/modelo/Categoria.java
@@ -0,0 +1,51 @@
+package br.grupointegrado.ads.picaretas.modelo;
+
+/**
+ * Clase onde representa a entidade CATEGORIA no Banco de Dados.
+ *
+ * @author Rhuan Coltre
+ */
+public class Categoria {
+
+    private int id;
+    private String descricao;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 53 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Categoria other = (Categoria) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+
+}
